0deb08f0f94ae2b59ba3b7587f64e39818750a3e
jae <55571956+jaevibing@users.noreply.github.com>
2022-11-20 21:09:35 +1100

    Update raspi.py

--- raspi.py
+++ raspi.py
@@ -1,19 +1,43 @@
-from flask import Flask,  Response, request
+from flask import Flask, request, jsonify
 import time
+import asyncio
 import RPi.GPIO as GPIO
 
-led = 11
+# references, not actual variables
+led = 18
+bigchungus = 11
+screamer = 13
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(led, GPIO.OUT)
 
 app = Flask(__name__)
 
-@app.route("/led", methods=["GET"])
-def led():
-    GPIO.output(led, GPIO.HIGH)
-    time.sleep(0.2)
-    GPIO.output(led, GPIO.LOW)
-    
+async def LED(i):
+    pin = 11
+    if i == 0:
+        pin = 18
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(pin, GPIO.LOW)
+
+@app.route("/toggleLED", methods=["GET"])
+def toggleLED():
+    LED(0)
+    return (jsonify(success=True))
+
+@app.route("/togglescream", methods=["GET"])
+def togglescream():
+    if request.args.get("isOn") == True:
+        GPIO.output(13, GPIO.LOW)
+    else:
+        GPIO.output(13, GPIO.HIGH)
+    return (jsonify(success=True))
+
+@app.route("/chungus", methods=["GET"])
+def chungus():
+    LED(1)
+    return (jsonify(success=True))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=80, host='0.0.0.0')
